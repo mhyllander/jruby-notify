bae580182e2a217b042a29818f6bb01373150078
Magnus Hyllander <mhy@peerialism.com>
2014-06-12 11:20:12 +0200

    pass detected change to callback

--- lib/jruby-notify/listener.rb
+++ lib/jruby-notify/listener.rb
@@ -7,19 +7,19 @@ module JRubyNotify
     end
 
     def file_renamed(watch, path, file, old)
-      clean_call(path, file, old)
+      clean_call(:renamed, path, file, old)
     end
 
     def file_modified(watch, path, file)
-      clean_call(path, file)
+      clean_call(:modified, path, file)
     end
 
     def file_deleted(watch, path, file)
-      clean_call(path, file)
+      clean_call(:deleted, path, file)
     end
 
     def file_created(watch, path, file)
-      clean_call(path, file)
+      clean_call(:created, path, file)
     end
 
     private
@@ -28,11 +28,11 @@ module JRubyNotify
     # Ensure that all paths have File::SEPARATOR as separator
     # This avoid a mix of different separators within the paths
     #
-    def clean_call(*paths)
+    def clean_call(change, *paths)
       if File::ALT_SEPARATOR
-        @callback.call paths.map { |p| p.tr(File::ALT_SEPARATOR, File::SEPARATOR) }
+        @callback.call change, paths.map { |p| p.tr(File::ALT_SEPARATOR, File::SEPARATOR) }
       else
-        @callback.call paths
+        @callback.call change, paths
       end
     end
 
